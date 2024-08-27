7da3654176c03ee915c9f772afa66c17e7bd1ad0
roser <roser_han@126.com>
2024-08-27 21:05:08 +0800

    - 实现 Vertex Shader 和 Fragment Shader 的编译和链接。 - 绘制三角形，绑定 Vertex Buffer Object 和 Vertex Array Object。

--- OpenGL/OpenGL/applicaiton.cpp
+++ OpenGL/OpenGL/applicaiton.cpp
@@ -34,6 +34,77 @@ int main(void)
         return -1;
     }
 
+    // Draw a triangle.
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    // Vertex array object.
+    unsigned int vertex_array_object;
+    glGenVertexArrays(1, &vertex_array_object);
+    glBindVertexArray(vertex_array_object);
+
+    // Vertex buffer.
+    unsigned int vertex_buffer;
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Attributes.
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Compile.
+    // Vertex shader.
+    const char* vertex_shader_source = "#version 330 core\n"
+                                     "layout (location = 0) in vec3 aPos;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                     "}\0";
+    unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+    int success;
+    char info_log[512];
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
+        std::cout << "ERROR:SHADER::VERTEX::COMPILATION_FAILED\n" << info_log << std::endl;
+    }
+    // Fragment shader.
+    const char* fragment_shader_source = "#version 330 core\n"
+									   "out vec4 FragColor;\n"
+									   "void main()\n"
+									   "{\n"
+									   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+									   "}\0";
+    unsigned int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+    glCompileShader(fragment_shader);
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
+        std::cout << "ERROR:SHADER::FRAGMENT::COMPILATION_FAILED\n" << info_log << std::endl;
+    }
+
+    // Link.
+    unsigned int shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+    
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shader_program, 512, NULL, info_log);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << info_log << std::endl;
+    }
+    // Delete shaders.
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
@@ -42,12 +113,23 @@ int main(void)
 
         processInput(window);
 
+        // Draw the triangle.
+        glUseProgram(shader_program);
+        // Because this code only draw one triangle, so we don't need to bind vertex array object every time.
+        // glBindVertexArray(vertex_array_object);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    // Optianl, de-allocate all resource once ther've outlived their purpose.
+    glDeleteVertexArrays(1, &vertex_array_object);
+    glDeleteBuffers(1, &vertex_buffer);
+    glDeleteProgram(shader_program);
 
     glfwTerminate();
     return 0;
