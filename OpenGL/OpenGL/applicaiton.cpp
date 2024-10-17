333f8961d794f56dadef3933548a893db02b6caa
roser <roser_han@126.com>
2024-10-17 09:15:41 +0800

    - 绘制矩形，使用 EBO 减少顶点数量。 - 重构代码，新增 Shader 类负责着色器相关工作。 - 使用纹理。

--- OpenGL/OpenGL/applicaiton.cpp
+++ OpenGL/OpenGL/applicaiton.cpp
@@ -2,10 +2,190 @@
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
+#include "shader.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+}
+
+unsigned int bindVertexArrayObject()
+{
+    // Vertex array object.
+    unsigned int vertex_array_object;
+    glGenVertexArrays(1, &vertex_array_object);
+    glBindVertexArray(vertex_array_object);
+    return vertex_array_object;
+}
+
+unsigned int bindVertexBufferObject(float vertices[], unsigned int vertices_size)
+{
+    // Vertex buffer object.
+    unsigned int vertex_buffer_object;
+    glGenBuffers(1, &vertex_buffer_object);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object);
+    glBufferData(GL_ARRAY_BUFFER, vertices_size, vertices, GL_STATIC_DRAW);
+    return vertex_buffer_object;
+}
+
+unsigned int bindElementBufferObject(unsigned int indices[], unsigned int indices_size)
+{
+    // Element Buffer object.
+    unsigned int element_buffer_object;
+    glGenBuffers(1, &element_buffer_object);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer_object);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices_size, indices, GL_STATIC_DRAW);
+    return element_buffer_object;
+}
+
+unsigned int generateTexture(const char* image_path, int active_texture, int rgb_mode)
+{
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glActiveTexture(active_texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Flip vertically.
+    stbi_set_flip_vertically_on_load(true);
+
+    // Load texture.
+    int width = 0;
+    int height = 0;
+    int nrChannels = 0;
+    unsigned char* data = stbi_load(image_path, &width, &height, &nrChannels, 0);
+    if (data != nullptr) {
+        glTexImage2D(GL_TEXTURE_2D, 0, rgb_mode, width, height, 0, rgb_mode, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture." << std::endl;
+    }
+    // Recycle the image data.
+    stbi_image_free(data);
+    return texture;
+}
+
+void drawTriangle(GLFWwindow* window, float vertices[], unsigned int vertice_size)
+{
+    // Vertex array object.
+    unsigned int vertex_array_object = bindVertexArrayObject();
+    // Vertex buffer object.
+    unsigned int vertex_buffer_object = bindVertexBufferObject(vertices, vertice_size);
+    // Attributes.
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Compile.
+    Shader shader("D:/Turotials/StudyOpenGL/OpenGL/OpenGL/shader.vs",
+                  "D:/Turotials/StudyOpenGL/OpenGL/OpenGL/shader.fs");
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        processInput(window);
+
+        // Draw the triangle.
+        shader.use();
+        // Because this code only draw one triangle, so we don't need to bind vertex array object every time.
+        //glBindVertexArray(vertex_array_object);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    // Optianl, de-allocate all resource once ther've outlived their purpose.
+    glDeleteVertexArrays(1, &vertex_array_object);
+    glDeleteBuffers(1, &vertex_buffer_object);
+}
+
+void drawRectangle(GLFWwindow* window)
+{
+    float vertices[] = {
+        // ---- 位置 ----      ---- 颜色 ----    -- 纹理 --
+        0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // 右上
+        0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // 右下
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,  // 左下
+        -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // 左上
+    };
+    unsigned int indices[] = {0, 1, 3, 1, 2, 3};
+
+
+    // Vertex array object.
+    unsigned int vertex_array_object = bindVertexArrayObject();
+
+    // Vertex buffer object.
+    unsigned int vertex_buffer_object = bindVertexBufferObject(vertices, sizeof(vertices));
+
+    // Element buffer object.
+    unsigned int element_buffer_object = bindElementBufferObject(indices, sizeof(indices));
+
+    // Texture.
+    unsigned int texture1 = generateTexture("D:\\Turotials\\StudyOpenGL\\OpenGL\\Assets\\container.jpg", GL_TEXTURE0, GL_RGB);
+    unsigned int texture2 = generateTexture("D:\\Turotials\\StudyOpenGL\\OpenGL\\Assets\\awesomeface.png", GL_TEXTURE1, GL_RGBA);
+
+    // Attributes.
+    // Vertex positions.
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // Vertex colors.
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // Texture.
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    // Compile.
+    Shader shader("D:/Turotials/StudyOpenGL/OpenGL/OpenGL/shader.vs",
+                  "D:/Turotials/StudyOpenGL/OpenGL/OpenGL/shader.fs");
+
+    shader.use();
+    shader.setInt("texture1", 0);
+    shader.setInt("texture2", 1);
+
+    //// Wireframe mode.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        processInput(window);
+
+        // Draw the triangle.
+        shader.use();
+        // Because this code only draw one triangle, so we don't need to bind vertex array object every time.
+        //glBindVertexArray(vertex_array_object);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    // Optianl, de-allocate all resource once ther've outlived their purpose.
+    glDeleteVertexArrays(1, &vertex_array_object);
+    glDeleteBuffers(1, &vertex_buffer_object);
+    glDeleteBuffers(1, &element_buffer_object);
 }
 
 int main(void)
@@ -34,102 +214,10 @@ int main(void)
         return -1;
     }
 
-    // Draw a triangle.
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
-    };
+    //float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+    //drawTriangle(window, vertices, sizeof(vertices));
 
-    // Vertex array object.
-    unsigned int vertex_array_object;
-    glGenVertexArrays(1, &vertex_array_object);
-    glBindVertexArray(vertex_array_object);
-
-    // Vertex buffer.
-    unsigned int vertex_buffer;
-    glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // Attributes.
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // Compile.
-    // Vertex shader.
-    const char* vertex_shader_source = "#version 330 core\n"
-                                     "layout (location = 0) in vec3 aPos;\n"
-                                     "void main()\n"
-                                     "{\n"
-                                     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                     "}\0";
-    unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
-    glCompileShader(vertex_shader);
-    int success;
-    char info_log[512];
-    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
-        std::cout << "ERROR:SHADER::VERTEX::COMPILATION_FAILED\n" << info_log << std::endl;
-    }
-    // Fragment shader.
-    const char* fragment_shader_source = "#version 330 core\n"
-									   "out vec4 FragColor;\n"
-									   "void main()\n"
-									   "{\n"
-									   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-									   "}\0";
-    unsigned int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
-    glCompileShader(fragment_shader);
-    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
-        std::cout << "ERROR:SHADER::FRAGMENT::COMPILATION_FAILED\n" << info_log << std::endl;
-    }
-
-    // Link.
-    unsigned int shader_program = glCreateProgram();
-    glAttachShader(shader_program, vertex_shader);
-    glAttachShader(shader_program, fragment_shader);
-    glLinkProgram(shader_program);
-    
-    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shader_program, 512, NULL, info_log);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << info_log << std::endl;
-    }
-    // Delete shaders.
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window)) {
-        /* Render here */
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        processInput(window);
-
-        // Draw the triangle.
-        glUseProgram(shader_program);
-        // Because this code only draw one triangle, so we don't need to bind vertex array object every time.
-        // glBindVertexArray(vertex_array_object);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    // Optianl, de-allocate all resource once ther've outlived their purpose.
-    glDeleteVertexArrays(1, &vertex_array_object);
-    glDeleteBuffers(1, &vertex_buffer);
-    glDeleteProgram(shader_program);
+    drawRectangle(window);
 
     glfwTerminate();
     return 0;
---
+++ OpenGL/OpenGL/shader.cpp
@@ -0,0 +1,97 @@
+#include "shader.h"
+
+Shader::Shader(const char* vertex_path, const char* fragment_path)
+{
+    std::string vertex_code;
+    std::string fragment_code;
+    std::ifstream vertex_shader_file;
+    std::ifstream fragment_shader_file;
+
+    vertex_shader_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fragment_shader_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+    try {
+        vertex_shader_file.open(vertex_path);
+        fragment_shader_file.open(fragment_path);
+
+        // Reading.
+        std::stringstream vertexShaderStream;
+        std::stringstream fragmentShaderStream;
+        vertexShaderStream << vertex_shader_file.rdbuf();
+        fragmentShaderStream << fragment_shader_file.rdbuf();
+
+        vertex_shader_file.close();
+        fragment_shader_file.close();
+
+        // Get the string of file.
+        vertex_code = vertexShaderStream.str();
+        fragment_code = fragmentShaderStream.str();
+    } catch (std::ifstream::failure e) {
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
+    }
+
+    const char* vertex_shader_code = vertex_code.c_str();
+    const char* fragment_shader_code = fragment_code.c_str();
+
+    int success;
+    char info_log[512];
+
+    // Compile vertex shader.
+    unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_code, NULL);
+    glCompileShader(vertex_shader);
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
+        std::cout << "ERROR:SHADER::VERTEX::COMPILATION_FAILED\n" << info_log << std::endl;
+    }
+    
+    // Compile fragment shader.
+    unsigned int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_code, NULL);
+    glCompileShader(fragment_shader);
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
+        std::cout << "ERROR:SHADER::FRAGMENT::COMPILATION_FAILED\n" << info_log << std::endl;
+    }
+
+    // Link the shader program.
+    shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shader_program, 512, NULL, info_log);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << info_log << std::endl;
+    }
+    // Delete shaders.
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+}
+
+Shader::~Shader() {
+    glDeleteProgram(shader_program);
+}
+
+void Shader::use()
+{
+    glUseProgram(shader_program);
+}
+
+void Shader::setBool(const std::string& name, bool value) const
+{
+    glUniform1i(glGetUniformLocation(shader_program, name.c_str()), (int)value);
+}
+
+void Shader::setInt(const std::string& name, int value) const
+{
+    glUniform1i(glGetUniformLocation(shader_program, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string& name, float value) const
+{
+    glUniform1f(glGetUniformLocation(shader_program, name.c_str()), value);
+}
