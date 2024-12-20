796af7b376653ba67d4973c00842434fc6fddf3e
roser <roser_han@126.com>
2024-12-20 11:14:30 +0800

    分离并创建平行光照和点光源光照GLSL函数。

--- OpenGL/OpenGL/lighting/box_shader.fs
+++ OpenGL/OpenGL/lighting/box_shader.fs
@@ -12,11 +12,16 @@ struct Material {
 	float shininess;
 };
 
-struct Light {
-	vec3 position;
+struct DirLight {
 	vec3 direction;
-	float cutOff;
-	float outerCutOff;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+}
+
+struct PointLight {
+	vec3 position;
 
 	vec3 ambient;
 	vec3 diffuse;
@@ -27,10 +32,50 @@ struct Light {
 	float quadratic;
 };
 
+#define NR_POINT_LIGHTS 4
+
 uniform Material material;
-uniform Light light;
+uniform DirLight dirLight;
+uniform PointLight pointLights[NR_POINT_LIGHTS];
 
 uniform vec3 viewPos;
+
+vec3 calcDirLight(DirLight light, vec3 normal, vec3 viewDir)
+{
+	vec3 lightDir = normalize(-light.direction);
+	float diff = max(dot(normal, lightDir), 0.0);
+
+	vec3 reflectDir = reflect(-lightDir, normal);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess;)
+
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+	vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+	return (ambient + diffuse + specular);
+}
+
+vec3 calcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
+{
+	vec3 lightDir = normalize(light.position - FragPos);
+
+	float diff = max(dot(normal, lightDir), 0.0);
+	
+	vec3 reflectDir = reflect(-lightDir, normal);
+	float spec = pow(max(dot(reflectDir, viewDir), 0.0), material.shininess);
+
+	float distance = length(light.position - FragPos);
+	float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+	vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords))
+
+	ambient *= attenuation;
+	diffuse *= attenuation;
+	specular *= attenuation;
+
+	return (ambient + diffuse + specular);
+}
 
 void main()
 {
